aaf7dc5011003bd877d43d03261700ee01da3c1e
unknown <aguilera.k02@gmail.com>
2023-02-16 09:01:01 -0500

    set up routes

--- server.js
+++ server.js
@@ -1,11 +1,22 @@
 const fs = require("fs")
 const express = require("express")
+const path = require("path")
+
 
 const app = express();
 
 PORT = 3001;
 
-app.use(express.static('public'))
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/notes", (req, res) => {
+    res.sendFile(path.join(__dirname, "/public/notes.html"))
+})
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "/public/index.html"))
+})
 
 app.listen(PORT, () => {
     console.log("Now Listening on Port " + PORT)
