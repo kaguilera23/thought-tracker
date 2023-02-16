98c2b4403c95462a7c7fb1e20da186f768759deb
unknown <aguilera.k02@gmail.com>
2023-02-16 15:45:24 -0500

    added get route to send json

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const fs = require("fs")
 const express = require("express")
 const path = require("path")
-
+const db = require("./db/db.json")
 
 const app = express();
 
@@ -12,6 +12,14 @@ app.use(express.urlencoded({ extended: true }));
 
 app.get("/notes", (req, res) => {
     res.sendFile(path.join(__dirname, "/public/notes.html"))
+})
+
+app.get("/api/notes", (req, res) => {
+    res.json(db)
+})
+
+app.post("/notes", (req, res) => {
+    console.log(req.body)
 })
 
 app.get("*", (req, res) => {
