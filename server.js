4fd9f30b89c44e2aa1c34ee54dc10547945be7fc
unknown <aguilera.k02@gmail.com>
2023-03-01 18:36:02 -0500

    can add notes

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const fs = require("fs")
 const express = require("express")
 const path = require("path")
 const db = require("./db/db.json")
+const util = require("util");
+const readDatabase = util.promisify(fs.readFile);
+
 
 const app = express();
 
@@ -9,18 +12,47 @@ PORT = 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
 
 app.get("/notes", (req, res) => {
     res.sendFile(path.join(__dirname, "/public/notes.html"))
 })
 
 app.get("/api/notes", (req, res) => {
-    res.json(db)
+    readDatabase("./db/db.json").then((data) => res.json(JSON.parse(data)));
 })
 
-app.post("/notes", (req, res) => {
-    console.log(req.body)
-})
+app.post("/api/notes", (req, res) => {
+    const {title, text} = req.body;
+
+
+    const note = {
+        title: title,
+        text: text,
+      };
+  
+  
+      // save the note to db
+      const file = "./db/db.json";
+      fs.readFile(file, "utf8", (err, data) => {
+        if (err) {
+          console.error(err);
+        } else {
+          const notesData = JSON.parse(data);
+          notesData.push(note);
+  
+          fs.writeFile(file, JSON.stringify(notesData), (err) => {
+            if (err) {
+              console.error(err);
+            }
+            console.log(`Your note has been added!`);
+            res.send(note);
+          });
+        }
+      });
+    
+  })
 
 app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "/public/index.html"))
